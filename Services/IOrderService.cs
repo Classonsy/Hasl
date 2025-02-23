3a94ce5a6cedc1f1914c66f867c16b168ab24ef0
HFX <94785894+Classonsy@users.noreply.github.com>
2025-02-23 22:33:46 +0300

    0.0

--- Services/IOrderService.cs
+++
@@ -1,7 +0,0 @@
-public interface IOrderService
-{
-  Task<Order> CreateOrderAsync(Order order);
-  Task<Order> GetOrderByIdAsync(int id);
-  Task UpdateOrderStatusAsync(int id, OrderStatus status);
-  Task<List<Order>> GetOrdersAsync();
-}
